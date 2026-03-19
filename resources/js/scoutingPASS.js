b17f1116668f59042aa2fc042628afdb117f752f
rubymarie5608 <rubymarie5608@gmail.com>
2026-03-18 20:30:06 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- resources/js/scoutingPASS.js
+++ resources/js/scoutingPASS.js
@@ -23,7 +23,7 @@ var options = {
 };
 
 // Built from the JSON
-var requiredFields = []; //["e", "m", "l", "r", "s", "as"];
+var requiredFields = []; //["e", "m", "r", "s", "t"];
 
 function addTimer(table, idx, name, data) {
   var row = table.insertRow(idx);
